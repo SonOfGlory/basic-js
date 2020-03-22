29f39da8f35036add4d33240526c8083fa3a9018
Denys Marytchak <denys.marytchak@gmail.com>
2020-03-22 21:12:20 +0100

    init: data-sample colution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,3 +1,10 @@
+// module.exports = function countCats(array) {
+//   array.flat().forEach(element => {
+//     if (element.contains('^^')) counter = counter + 1;
+//   });
+//   return counter;
+// };
+
 module.exports = function countCats(cats) {
-  return cats.flat().filter(element => element == '^^').length
+  return cats.flat().filter(element => element === '^^').length
 };
