2e30c04161be528f0440bd0ba75ba1daac6d96df
Denys Marytchak <denys.marytchak@gmail.com>
2020-03-22 21:36:11 +0100

    init: repeater

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,4 @@
 module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) return false
-  return members.filter(member => typeof member == 'string')
-    .map(member => member.trim()[0].toUpperCase()).sort().join('')
+  return members.filter(member => typeof member == 'string').map(member => member.trim()[0].toUpperCase()).sort().join('')
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,8 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(words, options={}) {
+  const { repeatTimes=1, separator='+',
+          addition, additionRepeatTimes=1, additionSeparator='|' } = options
+
+  const adds = addition === undefined ? '' :
+    Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator)
+  return Array(repeatTimes).fill(words+adds).join(separator)
 };
-  
